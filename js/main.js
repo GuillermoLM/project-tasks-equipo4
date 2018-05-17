b9573334e02dd0d317b282f7808e7170d03cb836
Enrique <qui_per32@hotmail.com>
2018-05-17 16:28:10 +0200

    Added structure and css. New proyect trello board

--- js/main.js
+++ js/main.js
@@ -1,47 +1,59 @@
-(function(){
+(function () {
 	//Variables
 	var list = document.getElementById("list"),
 		inputTask = document.getElementById("inputId"),
 		buttonTask = document.getElementById("buttonAdd");
-	
+
 	//Functions	
-	var addTask = function(){
-		var task = inputTask.value,	
+	var addTask = function () {
+		var task = inputTask.value.trim(),
 			newTask = document.createElement("div"),
 			link = document.createElement("span"),
+			btnX = document.createElement("button");
 			content = document.createTextNode(task);
-		
-		if (task === "") {//When the input is empty and you click add task
+			txt = document.createTextNode("\u00D7");
+
+		if (task === "") { //When the input is empty and you click add task
 			inputTask.setAttribute("placeholder", "Agrega una tarea valida");
 			inputTask.className = "error";
 			return false;
 		}
-		 
-		link.appendChild(content);//Add content to p
-		newTask.appendChild(link);//Add p to div
-		list.appendChild(newTask);//Add the new task
-		 
+
+		link.className += "removeItem";
+
+		btnX.appendChild(txt);
+		link.appendChild(btnX);
+
+		link.appendChild(content); //Add content to p
+		newTask.appendChild(link); //Add p to div
+		list.appendChild(newTask); //Add the new task
+
+
+		//Call Delete element
+		newTask.onclick = deleteItem;
+
+		btnX.addEventListener("click", deleteItem);
+
 		inputTask.value = "";
-		//Delete elements 
-		for (var i = 0; i <= list.children.length -1; i++) {
-			list.children[i].addEventListener("click", function () {
-			this.parentNode.removeChild(this);
-			});
-		}
 	};
+
+	function deleteItem(e) {
+		e.target.parentElement.removeChild(e.target.parent());
+	}
+
+
 	//Create check function		
-	var checkTask = function(){
+	var checkTask = function () {
 		inputTask.className = "";
 		inputTask.setAttribute("placeholder", "Agrega tu tarea");
 	};
 
-	buttonTask.addEventListener("click", addTask);//Add task
-	inputTask.addEventListener("click", checkTask);//Check task
+	buttonTask.addEventListener("click", addTask); //Add task
+	inputTask.addEventListener("click", checkTask); //Check task
 
-	document.body.onkeyup = function(e){
-		if(e.keyCode == 13){
-		  addTask();
+	document.body.onkeyup = function (e) {
+		if (e.keyCode == 13) {
+			addTask();
 		}
 	}
 })();
-
