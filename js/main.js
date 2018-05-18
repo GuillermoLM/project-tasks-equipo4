943c0f3bac75fa7325ab905de84db07da27bf8a8
Guillermo <you@example.com>
2018-05-18 10:22:58 +0200

    JS added: addTask and removeTask funcionality. Also button Enter for addList

--- js/main.js
+++ js/main.js
@@ -68,11 +68,10 @@ let createListTemplete = (text) =>
 			
 		</div>
 		<div class="addTask">
-			<input type="text">
+			<input class="inputNewTask" type="text">
 			<button class="buttonId">add Task</button>
 		</div>
 	</div>
-	
 	`
 let createTaskItemTemplate = (text) =>
 	`
@@ -84,63 +83,103 @@ let createTaskItemTemplate = (text) =>
 	</div>
 	`
 
-let addList = () => {
-	let input = document.getElementsByClassName('inputNewList')[0];
+let addList = () => 
+{
+	let inputList = document.getElementsByClassName('inputNewList')[0];
 	//Recoger el nombre de la lista
-	let listName = input.value.trim();
-	console.log(listName);
+	let listName = inputList.value.trim();
 
 	//Si no hay nombre no hagas nada
-	if (listName === "") {
+	if (listName === "") 
+	{
 		console.error('no valid list name');
 		return;
 	}
 	//Borrar el input
-	input.value = '';
+	inputList.value = '';
 	//Crear el nodo
 	let newList = createElementFromString(createListTemplete(listName));
 	//Inyectarlo
 	getEl('lists')[0].appendChild(newList);
-
-
-
-
 	// add listener to remove it
 	let removeListButtons = getEl('buttonDL')
 	
-	for (const el of removeListButtons) {
-		
-		
+	for (const el of removeListButtons) 
+	{
 		el.addEventListener('click', removeList)
 	}
-
-
-}
-let removeList = (e) => {
+	let addTaskButtons = getEl('buttonId')
 	
-	e.target.parentNode.parentNode.parentNode.remove();
+	for (const el of addTaskButtons) 
+	{
+		el.addEventListener('click', addTask)
+	}
+}
 
+let removeList = (e) => 
+{
+	e.target.parentNode.parentNode.parentNode.remove();
 };
 
-let callBackOn = () => {
+let addTask = (evento) => 
+{
+	let inputTask = evento.target.parentNode.children[0];
+	console.log(inputTask);
+	//Recoger el nombre de la lista
+	let taskName = inputTask.value.trim();
 
-
-	document.getElementsByClassName('addList')[0].addEventListener("click", addList);
-
-
+	//Si no hay nombre no hagas nada
+	if (taskName === "") 
+	{
+		console.error('no valid list name');
+		return;
+	}
+	//Borrar el input
+	inputTask.value = '';
+	//Crear el nodo
+	let newTask = createElementFromString(createTaskItemTemplate(taskName));
+	//Inyectarlo
+	let actualList = evento.target.parentNode.parentNode;
+	console.log(actualList);
+	actualList.appendChild(newTask);
+	// add listener to remove it
+	let removeTaskButtons = getEl('buttonDT')
+	
+	for (const element of removeTaskButtons) 
+	{
+		element.addEventListener('click', removeTask)
+	}
 }
+
+let removeTask = (a) =>
+{
+	a.target.parentNode.remove();
+};
+
+let callBackOn = () => 
+{
+	document.getElementsByClassName('addList')[0].addEventListener("click", addList);
+}
+
 document.addEventListener('DOMContentLoaded', callBackOn);
 
-function createElementFromString(htmlString) {
+function createElementFromString(htmlString) 
+{
 	var div = document.createElement('div');
 	div.innerHTML = htmlString.trim();
-
 	// Change this to div.childNodes to support multiple top-level nodes
 	return div.firstChild;
 }
 
-function getEl(cssSelector) {
+function getEl(cssSelector) 
+{
 	return document.getElementsByClassName(cssSelector);
+}
+
+document.body.onkeyup = function (e) {
+	if (e.keyCode == 13) {
+		addList();
+	}
 }
 
 let $ = document.querySelectorAll;
